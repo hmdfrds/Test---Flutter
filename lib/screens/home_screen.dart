3b65e33f634199d020f4125b479ee01e06e16efd
Firdaus <firdaus1337@gmail.com>
2025-02-17 22:01:00 +0800

    update the selected item

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -50,14 +50,20 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Text("History"),
             ),
             DropdownButton(
-                items: getDropdownSelection()
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                onChanged: (newValue) {})
+              value: activityProvider.selectedItem,
+              items: getDropdownSelection()
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (newValue) {
+                setState(() {
+                  activityProvider.updateSelectedItem(newValue);
+                });
+              },
+            ),
           ],
         ),
       ),
