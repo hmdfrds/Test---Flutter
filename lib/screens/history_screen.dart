18f0485407bbd4a4f6fa5e9719ba745a15fcf035
Firdaus <firdaus1337@gmail.com>
2025-02-17 21:22:32 +0800

    make the history screen sort and only show 50 max

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -7,16 +7,21 @@ class HistoryScreen extends StatelessWidget {
   const HistoryScreen({super.key});
   @override
   Widget build(BuildContext context) {
+    final activities = Provider.of<ActivityProvider>(context)
+        .activities
+        .reversed
+        .toList()
+        .sublist(0, 50);
+
     return Scaffold(
       appBar: AppBar(title: Text('History')),
       body: ListView.builder(
         itemCount: Provider.of<ActivityProvider>(context).activities.length,
         itemBuilder: (context, index) {
-          final activity =
-              Provider.of<ActivityProvider>(context).activities[index];
+          final activity = activities[index];
           return ListTile(
             title: Text(activity.activity),
-            subtitle: Text(activity.price.toString()),
+            subtitle: Text("Price: ${activity.price.toString()}"),
           );
         },
       ),
